656ea0bd0a069a52953c1269652739164a8430bc
John Andersen <johnandersenpdx@gmail.com>
2015-07-13 23:37:01 -0700

    helpfull links to readme

--- README.md
+++ README.md
@@ -21,3 +21,10 @@ The build executable and source assembly file can be changed in make.sh
 To Do
 ---
 Add arguments to launched programs!
+
+Helpful links
+---
+1. http://www.tenouk.com/Bufferoverflowc/Bufferoverflow6.html
+2. http://insecure.org/stf/smashstack.html
+3. https://www.offensive-security.com/metasploit-unleashed/binary-payloads/
+4. https://gist.github.com/pdxjohnny/05439e9c54cbd04c0686 (oringinal of this)
--- sh.s
+++ sh.s
@@ -4,16 +4,17 @@
 	.type	main, @function
 
 main:
-	subl	$40, %esp
-	movl 	$0x00000068, 28(%esp)
-	movl 	$0x7361622f, 24(%esp)
-	movl 	$0x6e69622f, 20(%esp)
-	lea 	20(%esp), %edx
-	movl	%edx, 32(%esp)
-	movl	$0, 36(%esp)
-	leal	32(%esp), %ecx
+	subl	$48, %esp
+	movl 	$0x006e6f68, 36(%esp)
+	movl 	$0x7479702f, 32(%esp)
+	movl 	$0x6e69622f, 28(%esp)
+	movl 	$0x7273752f, 24(%esp)
+	lea 	24(%esp), %edx
+	movl	%edx, 40(%esp)
+	movl	$0, 44(%esp)
+	leal	40(%esp), %ecx
 	mov     $0,%edx
-	mov     32(%esp),%ebx
+	mov     40(%esp),%ebx
 	mov     $11,%eax
 	int     $0x80
 	movl    $0x1, %eax
